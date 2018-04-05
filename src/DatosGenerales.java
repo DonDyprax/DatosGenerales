6f079770078d37146a0c325b076482d0c5176605
franc <franc@Charlie>
2018-04-04 21:05:12 -0600

    Clase con Main para imprimir los datos generales

---
+++ src/DatosGenerales.java
@@ -0,0 +1,7 @@
+
+public class DatosGenerales {
+
+    public static void main(String[] args) {
+        System.out.println("Juan Pablo Acosta Valdivieso; 20 anios; 10/03/98; 00128917");
+    }    
+}
